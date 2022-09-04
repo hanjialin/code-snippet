aa20501ac8f47ddffc0b2eaa907f105f64976f0d
hanjialin <hanjialin1991@hotmail.com>
2022-09-04 13:08:55 +0800

    first commit

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,7 +4,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('@/view/home/home.vue') // 注意这里要带上 文件后缀.vue
+    component: () => import('@/view/home/home.vue')
   },
   {
     path: '/magicMenu-01',
