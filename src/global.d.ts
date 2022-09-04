db97c69a455bf531b3484479ca9449528ae9fbf8
hanjialin <hanjialin1991@hotmail.com>
2022-09-04 13:03:01 +0800

    first commit

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+export {}
+
+declare global {
+  interface Window {
+    $message: import('naive-ui').MessageApi
+    $dialog: import('naive-ui').DialogApi
+    $notification: import('naive-ui').NotificationApi
+    $loadingBar: import('naive-ui').LoadingBarApi
+  }
+}
